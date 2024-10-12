511b532f060dc4ec23ab0f7ad89bd69aac24baa1
WayOfTheDev <dibaker.man@yandex.ru>
2024-10-12 12:41:28 +0300

    Revert "Reload project"
    This reverts commit 58a200e2b58cf00a9dfbd6566889aefed97e89fb.

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by D. K. on 01.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
