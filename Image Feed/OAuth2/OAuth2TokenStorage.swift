4955f9bddb82674fb67fa170aacae7399ac9b44a
WayOfTheDev <dibaker.man@yandex.ru>
2024-10-20 22:59:59 +0300

    Спринт 11. Keychain
    Замена хранения данных, на безопасный способ с помощью Keychain

--- Image Feed/OAuth2/OAuth2TokenStorage.swift
+++ Image Feed/OAuth2/OAuth2TokenStorage.swift
@@ -1,23 +1,34 @@
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
     // MARK: - Singleton
     static let shared = OAuth2TokenStorage()
 
-    private let storage = UserDefaults.standard
-
     // MARK: - Properties
     var token: String? {
         get {
-            let token = storage.string(forKey: "OAuthToken")
-            return token
+            return KeychainWrapper.standard.string(forKey: "OAuthToken")
         }
         set {
-            storage.set(newValue, forKey: "OAuthToken")
             if let token = newValue {
-                print("Token saved to storage: \(token)")
+                let saveSuccessful: Bool = KeychainWrapper.standard.set(token, forKey: "OAuthToken")
+                #if DEBUG
+                if saveSuccessful {
+                    print("Token successfully saved to Keychain.")
+                } else {
+                    print("Failed to save token to Keychain.")
+                }
+                #endif
             } else {
-                print("Token removed from storage")
+                let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: "OAuthToken")
+                #if DEBUG
+                if removeSuccessful {
+                    print("Token successfully removed from Keychain.")
+                } else {
+                    print("Failed to remove token from Keychain.")
+                }
+                #endif
             }
         }
     }
